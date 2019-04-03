407d70d0eaf66110c4c4a27d478a91e18582ac38
Debatent <alexandreadrien@orange.fr>
2019-04-03 19:28:17 +0200

    Client: Systeme d'échange émetteur récepteur créé

--- client.c
+++ client.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 
 int connection (int sock){
@@ -41,13 +42,74 @@ int connection (int sock){
     return 0;
 }
 
+
 int main (void){
+
+    char reponse[2];
+    bool emetteur;
+    printf("Tapez 1 si vous voulez émettre, 0 si vous voulez recevoir:\n");
+    fgets(reponse,sizeof(reponse) ,stdin);
+    if (strcmp(reponse,"1")==0){
+        emetteur = true;
+    }
+    else{
+        emetteur = false;
+    }
+    printf("Vous êtes ");
+    if (emetteur){
+        printf("émetteur\n");
+    }
+    else{
+        printf("recepteur\n");
+    }
 
     int dSock = socket (PF_INET, SOCK_STREAM, 0);
     int res = connection(dSock);
     if (res == -1){
-        printf("Erreur: La connection a échoué");
+        printf("Erreur: La connection a échoué\n");
         return -1;
     }
+
+    bool continuer = true;
+    char message[280];
+
+    while(continuer){
+        if (emetteur){
+            printf("Tapez le message en 280 caractères:\n");
+            fgets(message, 280, stdin);
+            char * pos1 = strchr(message,'\n');
+            *pos1 ='\0';
+
+            res = send(dSock,message, strlen(message)+1,0);
+            if (res == -1){
+                printf("Erreur: Le message n'a pas été envoyé\n");
+                break;
+            }
+            if (strcmp(message,"fin")==0){
+                continuer = false;
+            }
+            emetteur = false;
+        }
+
+        else{
+            printf("En attente de message\n");
+            res = recv(dSock, message, sizeof(message),0);
+            if (res == 0){
+                printf("Warning: Serveur déconnecté\n");
+                break;
+            }
+            else if(res == -1){
+                printf("Erreur: Pas de message reçus\n");
+                break;
+            }
+            printf("Reçu: \n%s\n", message);
+            if (strcmp(message,"fin") == 0){
+                continuer = false;
+            }
+            emetteur = true;
+        }
+    }
+    close(dSock);
+    printf("Déconnection\n");
     return 0;
 }
