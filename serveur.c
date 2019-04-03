deec23d49e95bd8b0266f338b96d729b309f99f4
Etudiant IG <polig@gtp5-pc05.isim.intra>
2019-04-03 17:14:09 +0200

    Modif serveur

--- serveur.c
+++ serveur.c
@@ -5,26 +5,62 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <string.h>
 
+
+int quit = 0;
+
+void traitement(int n) {
+    quit = 1;
+}
 
 int main(void) {
+    /* Déclaration des variables */
+    int dS, res, dSC, dSC2, stop = 1;
+
+    char msg[280];
+
+
     /* Initialisation du serveur */
-    int dS = socket(PF_INET, SOCK_STREAM, 0);
+    dS = socket(PF_INET, SOCK_STREAM, 0);
     struct sockaddr_in ad;
     ad.sin_family = AF_INET;
-    ad.sin_addr.s_addr = INADDR_ANY;
-    ad.sin_port = 0; 
-    socklen_t len = sizeof(ad);
+    ad.sin_addr.s_addr = INADDR_ANY;  
+    res = bind(dS, (struct sockaddr*) &ad, sizeof(ad));
+    /* */
+    printf("%d\n", res);
+    printf("Port : %d\n", ntohs(ad.sin_port));
 
-    /* Assignation d'un port aléatoire */
-    if (getsockname(dS, (struct sockaddr*) &ad, &len) < 0) {
-        printf("Erreur lors de l'initialisation");
-        exit(1);
-    } else {
-        printf("Adresse IP : %s\n", inet_ntoa(ad.sin_addr));
-        printf("Port : %d\n", ntohs(ad.sin_port));
+    /**/  
+    listen(dS, 10);
+    struct sockaddr_in aC;
+    socklen_t lg = sizeof(struct sockaddr_in);
+    /* Tant que control+C n'est pas préssé pour arrêter le serveur */
+    while (quit != 1) {
+        /* Mise en place de la communication avec les 2 clients */
+        dSC = accept(dS, (struct sockaddr*) &aC, &lg);
+        dSC2 = accept(dS, (struct sockaddr*) &aC, &lg);
+        stop = 1;
+        /* Tant que la connexion n'a pas été perdue avec un des 2 clients */
+        while (stop > 0) {
+            /* Réception du message du client émetteur */
+            stop = recv(dSC, msg, strlen(msg), 0);
+            stop = send(dSC2, msg, strlen(msg), 0);
+            /* Réception du message du 2e client */
+            stop = recv(dSC2, msg, strlen(msg), 0);
+            stop = send(dSC, msg, strlen(msg), 0);
+            
+        }
+        stop = 1;
+        /* Fermeture de la communication avec les 2 clients */
+        close(dSC);
+        close(dSC2);
+        /* Check si un control+C a été lancé */
+        signal(SIGINT, traitement);
     }
-
+    /* Arrêt du serveur et fin */
+    close(dS);
     return 0;
      
 
