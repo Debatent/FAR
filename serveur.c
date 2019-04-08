4713080ba5f94be2358fa7f090774a90e477c117
Debatent <alexandreadrien@orange.fr>
2019-04-08 10:11:54 +0200

    Correction indentation

--- serveur.c
+++ serveur.c
@@ -26,71 +26,71 @@ int main(void) {
     dS = socket(PF_INET, SOCK_STREAM, 0);
     struct sockaddr_in ad;
     ad.sin_family = AF_INET;
-    ad.sin_addr.s_addr = INADDR_ANY;  
+    ad.sin_addr.s_addr = INADDR_ANY;
     ad.sin_port = 45001;
     res = bind(dS, (struct sockaddr*) &ad, sizeof(ad));
 
     /* Affichage du port */
     if (res == 0) {
-	printf("Initialisation du serveur terminée !\n");
+	    printf("Initialisation du serveur terminée !\n");
     } else {
-	return -1;
+	    return -1;
     }
     printf("Port : %d\n", ad.sin_port);
 
-    /* Attente des connections */  
+    /* Attente des connections */
     listen(dS, 10);
     struct sockaddr_in aC;
     socklen_t lg = sizeof(struct sockaddr_in);
 
     /* Boucle principale pour les connexions des clients */
     while (1) {
-	/* Condition d'arrêt du serveur */
-	if (deb == 1) {
-	    printf("Voulez vous arrêter le serveur ? o/n\n");
-	    if (getchar() == 'o') {
+	    /* Condition d'arrêt du serveur */
+	    if (deb == 1) {
+	        printf("Voulez vous arrêter le serveur ? o/n\n");
+	        if (getchar() == 'o') {
 	        break;
-	    }
+	        }
 	    vidermemoiretamponclavier();
-	}
-	deb = 1;
-	printf("En attente de connection ...\n");
+        }
+	    deb = 1;
+	    printf("En attente de connection ...\n");
         /* Mise en place de la communication avec les 2 clients */
         dSC = accept(dS, (struct sockaddr*) &aC, &lg);
         dSC2 = accept(dS, (struct sockaddr*) &aC, &lg);
 
         /* Boucle d'échange des messages entre les 2 clients */
         while (1) {
-	    bzero(msg, 280);
+	        bzero(msg, 280);
 
             /* Réception du message du client émetteur */
             recv(dSC, msg, sizeof(msg), 0);
-	    printf("Reçu : %s\n", msg);
+	        printf("Reçu : %s\n", msg);
 
-	    /* Envoi du message au second client */
-	    send(dSC2, msg, sizeof(msg), 0);
+	        /* Envoi du message au second client */
+	        send(dSC2, msg, sizeof(msg), 0);
 
-	    /* Condition d'arrêt de la communication (message = fin) */
-	    if (strcmp("fin", msg) == 0) {
-		printf("Fin de la communication\n");		
-		break;
-	    }
-	    printf("Message envoyé au second client...\n");
-	    bzero(msg, 280);
+	        /* Condition d'arrêt de la communication (message = fin) */
+	        if (strcmp("fin", msg) == 0) {
+		        printf("Fin de la communication\n");
+		        break;
+	        }
+	        printf("Message envoyé au second client...\n");
+	        bzero(msg, 280);
 
             /* Réception de la réponse du second client */
             recv(dSC2, msg, sizeof(msg), 0);
-	    printf("Reçu : %s\n", msg);
+	        printf("Reçu : %s\n", msg);
 
-	    /* Envoi de la réponse au premier client */
-	    send(dSC, msg, sizeof(msg), 0);
+	        /* Envoi de la réponse au premier client */
+	        send(dSC, msg, sizeof(msg), 0);
 
-	    /* Condition d'arrêt de la communication (message = fin) */
-	    if (strcmp("fin", msg) == 0) {
-		printf("Fin de la communication\n");		
-		break;
-	    }
-	    printf("Message envoyé au premier client...\n");
+	        /* Condition d'arrêt de la communication (message = fin) */
+	        if (strcmp("fin", msg) == 0) {
+		        printf("Fin de la communication\n");
+		        break;
+	        }
+	        printf("Message envoyé au premier client...\n");
         }
 
     }
@@ -99,5 +99,5 @@ int main(void) {
     printf("Arrêt du serveur...\n");
     close(dS);
     return 0;
-  
+
 }
