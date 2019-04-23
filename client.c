a58b7a4d7a5ae9176daa31e17e2a04bedce78976
Debatent <alexandreadrien@orange.fr>
2019-04-23 16:10:21 +0200

    Client: gestion doublon pseudo et structure fct

--- client.c
+++ client.c
@@ -79,6 +79,67 @@ int connexion (int sock){
 }
 
 
+int entrerpseudo(void* args){
+    /**
+     *Demande à l'utilisateur son pseudo, et le redemande si le pseudo est déjà pris
+     *Prend en entrée un message thread
+     *renvoie 0 si le pseudo a été validé, -1 en cas d'erreur
+     */
+     struct messagethread *argument = (struct messagethread*) args;
+     char pseudonyme[argument->taillepseudo];
+     char reponse[2];
+     int res;
+     while(1){
+         printf("Veuillez entrer votre pseudo en %d caractères maximum:\n",argument->taillepseudo);
+         fgets(pseudonyme, argument->taillepseudo + 1, stdin);
+
+         char * pos1 = strchr(pseudonyme,'\n');
+         *pos1 ='\0';
+
+         res = send (argument->dSock, pseudonyme,strlen(pseudonyme) + 1,0);
+         if (res<0){
+             return -1;
+         }
+         res = recv(argument->dSock, reponse,strlen(reponse) + 1,0);
+         if (res<=0){
+             return-1;
+         }
+         else if (strcmp(reponse, "-1")){
+             puts("Pseudo déjà pris par quelqu'un d'autre");
+         }
+         else{
+             printf("Bienvenue %s\n",pseudonyme);
+             break;
+         }
+     }
+     return 0;
+}
+
+
+void* envoyerdestinataire(void* args){
+    /**
+     *Demande et envoie le pseudo du recepteur
+     */
+    printf("Entrez le pseudo du destinataire");
+}
+
+void* envoyerfichier(void* args){
+    /**
+     *affiche les nom des fichiers du répertoire files
+     *Demande à l'émetteur le nom du fichier
+     *envoie le titre puis le contenu du fichier selectionné au recepteur
+     */
+}
+
+
+void* recevoirfichier(void* args){
+    /**
+     *reçoit le titre puis le contenu du fichier qu'on place dans le repertoire
+     *reception
+     */
+}
+
+
 
 void *recevoirmessage(void* args){
     /**
@@ -138,6 +199,9 @@ void *envoyermessage(void* args){
             printf("Fin de l'envoi des messages\n");
             break;
         }
+        else if (strcmp(msg,"file")){
+
+        }
     }
     return NULL;
 }
@@ -170,15 +234,13 @@ int main (void){
         msgthr.taillepseudo = 12;
 
     /*Saisie du pseudo et envoie au Serveur*/
-    char pseudonyme[msgthr.taillepseudo];
-    printf("Veuillez entrer votre pseudo en %d caractères maximum:\n",msgthr.taillepseudo);
-    fgets(pseudonyme, msgthr.taillepseudo + 1, stdin);
-
-    char * pos1 = strchr(pseudonyme,'\n');
-    *pos1 ='\0';
-
-    send (dSock, pseudonyme,strlen(pseudonyme) + 1,0);
-
+    res = entrerpseudo((void*)&msgthr.taillepseudo);
+    if (res<0){
+        puts("Erreur de communication avec le serveur");
+        close(dSock);
+        puts("Arret");
+        return 0;
+    }
 
     /*Création des threads d'envoi et de reception*/
     pthread_t recepteur;
