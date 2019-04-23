a23d42fd00f0a2b3d4bda80fa11d11167a727f4b
Etudiant IG <polig@gtp5-pc05.isim.intra>
2019-04-23 16:25:27 +0200

    bug

--- serveur.c
+++ serveur.c
@@ -15,14 +15,12 @@
 
 int dS;
 
-void traitement ( int n){
+/* Traitement du signal CTRL+C (condition d'arrêt du serveur) */
+void traitement (int n){
     close(dS);
     puts("Arret serveur");
     _exit(0);
 }
-/* Déclaration du tableau de sockets et remplissage avec des 0 */
-int tabSockets[TAILLEMAX] = { 0 };
-
 
 void vidermemoiretamponclavier(void)
 {
@@ -35,36 +33,52 @@ void vidermemoiretamponclavier(void)
 struct thread_args {
     int dSC;
     char pseudo[280];
+    struct sockaddr_in aC;
 };
 
+/* Déclaration du tableau de sockets et remplissage avec des 0 */
+struct thread_args tabSockets[TAILLEMAX] = { 0 };
+
+/* Renvoi le nombre de clients connectés à la discussion */
 int getNbConnectes() {
     int count = 0;
     for (int i = 0; i < TAILLEMAX-1; i++) {
-        if (tabSockets[i] != 0) {
+        if (tabSockets[i].dSC != 0) {
             count++;
         }
     }
     return count;
 }
 
+int getPseudo(char * pseudo) {
+    printf("%s\n", pseudo);
+    for (int i = 0; i < TAILLEMAX-1; i++) {
+        if (strcmp(tabSockets[i].pseudo, pseudo) == 0) {
+            return 0;
+        }
+    }
+    return -1;
+}
+
 /* Thread qui réceptionne le message d'un client et l'envoie à l'autre en boucle  */
-void *threadEnvoi(void *args) {
+void *threadEnvoi(void * numCli) {
     char msg[280];
     char messageComplet[280];
-    int res;
+    int res, fin = 0;
     char pseudo[280];
-    int dSC;
-    /* Récupération des arguments */
-    struct thread_args *arguments = (struct thread_args *) args;
-    /* Réception du pseudo */
-    dSC = arguments->dSC;
-    recv(dSC, pseudo, sizeof(pseudo), 0);
+    int num = *((int *) numCli);
+    printf("%d\n", num);
+    int dSC = tabSockets[num].dSC;
+
+
     printf("Pseudo : %s\n", pseudo);
+    printf("ID du client : %d\n", dSC);
+    printf("Adresse IP : %s\n", inet_ntoa(tabSockets[num].aC.sin_addr));
+    printf("Port : %d\n",(int) ntohs(tabSockets[num].aC.sin_port));
 
     /* Condition d'arrêt : Pas de réception (le client a mis fin à la connexion) */
     while (1) {
         /* En attente de réception d'un message */
-        printf("ENTREE DANS LE THREAD AVEC DSC : %d\n", dSC);
         res = recv(dSC, msg, sizeof(msg),0);
         if (res == 0) {
             break;
@@ -76,20 +90,25 @@ void *threadEnvoi(void *args) {
             strcat(messageComplet, " a quitté la discussion\0");
             /* Envoi à tous les clients */
             for (int i =0; i < TAILLEMAX-1; i++) {
-                if (tabSockets[i] != 0) {
+                if (tabSockets[i].dSC != 0) {
                     /* Suppression du socket du client ayant émit "fin" dans le tableau */
-                    if (tabSockets[i] == dSC) {
-                        tabSockets[i] = 0;
+                    if (tabSockets[i].dSC == dSC) {
+                        tabSockets[i].dSC = 0;
                     } else {
                         printf("ENVOI AU CLIENT %d\n", i);
-                        res = send(tabSockets[i], messageComplet, sizeof(messageComplet), 0);
+                        res = send(tabSockets[i].dSC, messageComplet, sizeof(messageComplet), 0);
                         if (res == 0) {
-                            tabSockets[i] = 0;
+                            tabSockets[i].dSC = 0;
                         }
                     }
                 }
             }
             break;
+        /* Si le message reçu est "file" */
+        } else if (strcmp(msg, "file") == 0) {
+            /* Récupération du pseudo du client auquel le client veut envoyer le fichier */
+            
+
         }
         /* Formattage du message à envoyer */
         strcpy(messageComplet, pseudo);
@@ -98,11 +117,11 @@ void *threadEnvoi(void *args) {
         printf("%s\n", messageComplet);
         /* Envoi à tous les clients */
         for (int i = 0; i < TAILLEMAX-1; i++) {
-            if (tabSockets[i] != 0 && tabSockets[i] != dSC) {
-                printf("ENVOI AU CLIENT %d\n", tabSockets[i]);
-                res = send(tabSockets[i], messageComplet, sizeof(messageComplet), 0);
+            if (tabSockets[i].dSC != 0 && tabSockets[i].dSC != dSC) {
+                printf("ENVOI AU CLIENT %d\n", tabSockets[i].dSC);
+                res = send(tabSockets[i].dSC, messageComplet, sizeof(messageComplet), 0);
                 if (res == 0) {
-                    tabSockets[i] = 0;
+                    tabSockets[i].dSC = 0;
                 }
             }
         }
@@ -118,9 +137,6 @@ int main(void)
     /* Déclaration des variables */
     int res, i=0;
     pthread_t tid[TAILLEMAX];
-
-    /* Déclaration des structures */
-    struct thread_args arguments;
 
     /* Initialisation du serveur */
     dS = socket(PF_INET, SOCK_STREAM, 0);
@@ -147,22 +163,21 @@ int main(void)
     struct sockaddr_in aC;
     socklen_t lg = sizeof(struct sockaddr_in);
     char msg[280];
-
+    int *arg;
 
     signal(SIGINT,traitement);
     /* Boucle principale pour les connexions des clients */
     while (1)
     {
         /* Attente de connexions entrantes */
-        arguments.dSC = accept(dS, (struct sockaddr *)&aC, &lg);
-        printf("Client DSC : %d\n", arguments.dSC);
-
-        /* Ajout du socket du client au tableau de sockets */
-        tabSockets[i] = arguments.dSC;
+        tabSockets[i].dSC = accept(dS, (struct sockaddr *)&aC, &lg);
+        tabSockets[i].aC = aC;
+        printf("Client DSC : %d\n", tabSockets[i].dSC);
         sprintf(msg, "Il y a actuellement %d connecté(s)", getNbConnectes());
-        send(arguments.dSC, msg, sizeof(msg), 0);
+        send(tabSockets[i].dSC, msg, sizeof(msg), 0);
         /* Création du thread permettant la transmission des messages */
-        pthread_create(&tid[i], NULL, threadEnvoi, (void *)&arguments);
+        *arg = i;
+        pthread_create(&tid[i], NULL, threadEnvoi, arg);
         i++;
 
     }
